7ba94cf8872335be214da380ed461aea3f8b969f
maxwellkoch <maxtheskier@icloud.com>
2025-10-24 13:01:39 -0600

    Updated problem up to controllable decomposition.

--- homework/homework_7/CreateSegwayControl.m
+++ homework/homework_7/CreateSegwayControl.m
@@ -65,27 +65,33 @@ f = [v*cos(psi); v*sin(psi); omega; omegadot; vdot; phidot; phiddot];
 df_dx = jacobian(f, X);
 df_du = jacobian(f, U);
 
-% %% Linearize Equations about x = 0
-% % Set the states to zero and evaluate the dynamics at the zero state and control
-% zero_state_f = subs(f, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]);
-% 
-% if zero_state_f == 0
-%     disp('Zero-state and zero input is an equilibrium point.')
-%     disp('')
-% end
-% % Create the state matrices (A,B) from df_dx and df_du
-% A = subs(df_dx, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]); 
-% B = subs(df_du, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]);
-% 
-% % Calculate controllability
-% gamma = ctrb(A, B);
-% gamma_rank = rank(gamma);
-% [n, ~] = size(A); 
-% if gamma_rank == n
-%     disp('The system is controllable.');
-% else
-%     disp('The system is not controllable.');
-% end
+%% Linearize Equations about x = 0
+% Set the states to zero and evaluate the dynamics at the zero state and control
+zero_state_f = subs(f, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+if zero_state_f == 0
+    disp('Zero-state and zero input is an equilibrium point.')
+else 
+    disp('Zero-state and zero input is not an equilibrium point.')
+end
+disp(' ')
+
+% Create the state matrices (A,B) from df_dx and df_du
+A_sym = df_dx;
+B_sym = df_du;
+A = subs(df_dx, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]); 
+B = subs(df_du, [x, y, psi, omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+% Calculate controllability
+gamma = ctrb(A, B);
+gamma_rank = rank(gamma);
+[n, ~] = size(A); 
+if gamma_rank == n
+    disp('The system is completely controllable.');
+else
+    disp('The system is not completely controllable.');
+end
+disp(' ')
 
 %% Linearize Equations about z = 0 (Note, you will want to comment out the previous section so that you 
 %%                                  are still working with symbolic variables at this point)
@@ -95,18 +101,39 @@ fz = [omegadot; vdot; phidot; phiddot];
 
 % Calculate the jacobians (use the dynamics of the z state to calculate dfz/dz and dfz/du)
 dfz_dx = jacobian(fz, z); 
-dfz_dx = jacobian(fz, U);
+dfz_du = jacobian(fz, U);
 
 % Set the states to zero
 % Show that this is an equilibrium
 zero_state_fz = subs(fz, [omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0]);
 if zero_state_fz == 0
     disp('Reduced state at zero-state and zero input is an eq point.')
+else 
+    disp('Reduced state at zero-state and zero input is not an eq point.')
 end 
+disp(' ')
 
 % Create the state matrices (A,B) from df_dx and df_du
-A = subs(dfz_dx, [])
+
+Az = subs(dfz_dx, [omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0]);
+Bz = subs(dfz_du, [omega, v, phi, phidot, u1, u2], [0, 0, 0, 0, 0, 0]);
+
 % Evaluate controllability
+gamma_reduced_state = ctrb(Az, Bz);
+gamma_reduced_rank = rank(gamma_reduced_state);
+[n,~] = size(Az);
+if gamma_reduced_rank == n 
+    disp('The reduced state is completely controllable.')
+else
+    disp('The reduced state is not completely controllable.')    
+end 
+disp(' ')
+
+% Explain why z is controllable in isolation from the other states
+% Build a T matrix for controllability decomposition
+
+
+
 
 %% Create a stabilizing control
 
