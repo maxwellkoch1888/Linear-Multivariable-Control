a44a791fe550f32dcc915af3da6be08df1c393e1
maxwellkoch <maxtheskier@icloud.com>
2025-11-20 21:26:15 -0700

    Partway through homework 11.

--- homework/homework_10/state_space_integral_control_design.m
+++ homework/homework_10/state_space_integral_control_design.m
@@ -1,6 +1,6 @@
 function state_space_integral_control_design()
-    % problem_A()
-    problem_B()
+    problem_A()
+    % problem_B()
 
 end 
 
@@ -25,12 +25,12 @@ function problem_A()
 
     % BUILD Q AND R WITH BRYSONS METHOD
     xmax = [1/9, 1/16, 1/100];
-    umax = [1/4];
+    umax = [1/9];
     Q = diag(xmax);
     R = diag(umax);
     
     % FIND GAIN MATRIX
-    k = lqr(A, B, Q, R);
+    k = lqr(A, B, Q, R)
 
     % % FIND STEADY STATE
     % xd = [-2;0;0];
